21301a5385aa8b807764c1be969d88dffd439316
韩紫嫣 <blodcell@163.com>
2021-01-02 09:29:37 +0800

    修复resize不同步问题

--- wjjol/js/3d.js
+++ wjjol/js/3d.js
@@ -249,8 +249,8 @@ window.onresize = function() {
 		canvasSizeX = 1200;
 		canvasSizeY = 960;
 	} else if (window.innerWidth > 1350) {
-		canvasSizeX = 800;
-		canvasSizeY = 640;
+		canvasSizeX = 900;
+		canvasSizeY = 720;
 	} else if (window.innerWidth > 1200) {
 		canvasSizeX = 600;
 		canvasSizeY = 480;

0daedcddf8da30811fa00c2d3760cbda10c6eefa
Arthur19312 <blodcell@163.com>
2021-01-22 09:01:41 +0800

    lab目录调整 Git下标闪烁

--- wjjol/js/index.js
+++ wjjol/js/index.js
@@ -202,134 +202,139 @@ emojiBoard.hover(
 //github
 let gitFlexBox = $('.git-content')[0]
 let gitNowActive = 1
-let gitSub = [0,$('#gitSub1'), $('#gitSub2'), $('#gitSub3'), $('#gitSub4')]
+let gitSub = [0, $('#gitSub1'), $('#gitSub2'), $('#gitSub3'), $('#gitSub4')]
 
 /* document.addEventListener('DOMMouseScroll',handler,false)
     document.addEventListener('mousewheel',handler,false) */
-	document.addEventListener('DOMMouseScroll',throttle(realFunc,0,200),false)
-	    document.addEventListener('mousewheel',throttle(realFunc,0,200),false)
-	
-	
-	function handler(event){
-			var detail = event.wheelDelta || event.detail;
-			var moveForwardStep = 1;
-			var moveBackStep = -1;
-			var step = 0;	
-			if(detail > 0){
-					step = moveForwardStep*600;
-			}else{
-				step = moveBackStep *600;
-			}
-		} 
-		
-		
-		function throttle(func, wait, mustRun) {
-		    var timeout,
-		        startTime = new Date();
-		 
-		    return function() {
-		        var context = this,
-		            args = arguments,
-		            curTime = new Date();
-		 
-		        clearTimeout(timeout);
-		        // 如果达到了规定的触发时间间隔，触发 handler
-		        if(curTime - startTime >= mustRun){
-		            func.apply(context,args);
-		            startTime = curTime;
-		        // 没达到触发间隔，重新设定定时器
-		        }else{
-		            //timeout = setTimeout(func, wait);
-		        }
-		    };
-		};
-		// 实际想绑定在 scroll 事件上的 handler
-		function realFunc(){
-			var detail = event.wheelDelta || event.detail;
-			var moveForwardStep = 1;
-			var moveBackStep = -1;
-			var step = 0;	
-			if(detail > 0){
-					step = moveForwardStep*400;
-			}else{
-				step = moveBackStep *400;
-			}
-			let str = gitFlexBox.scrollLeft - step
-		    gitFlexBox.scrollTo({
-		      left: str,
-		      behavior: 'smooth'
-		    });
-			
-			if((str > 0 ) && (str <= 1000)){
-				gitNowActive = 1
-				
-			}else if((str > 1000 ) && (str <= 1700)){
-				gitNowActive = 2
-				
-			}else if((str > 1700 ) && (str <= 2700)){
-				gitNowActive = 3
-				
-			}else if(str > 2700){
-				gitNowActive = 4
-				
-			}else{
-				console.log(gitNowActive+'gna')
-			}
-			
-			gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
-			gitSub[gitNowActive].addClass('sub-active');
-			
+document.addEventListener('DOMMouseScroll', throttle(realFunc, 0, 200), false)
+document.addEventListener('mousewheel', throttle(realFunc, 0, 200), false)
+
+
+function handler(event) {
+	var detail = event.wheelDelta || event.detail;
+	var moveForwardStep = 1;
+	var moveBackStep = -1;
+	var step = 0;
+	if (detail > 0) {
+		step = moveForwardStep * 600;
+	} else {
+		step = moveBackStep * 600;
+	}
+}
+
+
+function throttle(func, wait, mustRun) {
+	var timeout,
+		startTime = new Date();
+
+	return function() {
+		var context = this,
+			args = arguments,
+			curTime = new Date();
+
+		clearTimeout(timeout);
+		// 如果达到了规定的触发时间间隔，触发 handler
+		if (curTime - startTime >= mustRun) {
+			func.apply(context, args);
+			startTime = curTime;
+			// 没达到触发间隔，重新设定定时器
+		} else {
+			//timeout = setTimeout(func, wait);
 		}
-		// 采用了节流函数
-		//window.addEventListener('scroll',throttle(realFunc,500,1000));
-		
-		
-		
-		function scrollGit(n){
-			gitFlexBox.scrollTo({
-			  left: n===1?0:n===2?1350:n===3?2400:n===4?3600:0,
-			  behavior: 'smooth'
-			});
-			
-			gitNowActive = n;
-			gitSub[n].siblings('.sub-active').removeClass('sub-active')
-			gitSub[n].addClass('sub-active');
-		}
-		
-		
-		
-		let gitContent = $('#github')
-		
-		$(document).keydown((event)=>{
-			let kc = event.keyCode
-			if(gitContent.hasClass('content-item-active')){
-				if((kc === 37) || (kc === 65)){
-					if(gitNowActive != 1){
-						gitNowActive--;
-						scrollGit(gitNowActive)
-						gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
-						gitSub[gitNowActive].addClass('sub-active');
-					}
-				}else if((kc === 39)||(kc === 68)){
-					if(gitNowActive != 4){
-						gitNowActive++;
-						scrollGit(gitNowActive)
-						gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
-						gitSub[gitNowActive].addClass('sub-active');
-					}
-				}else{}
-				
-				
+	};
+};
+// 实际想绑定在 scroll 事件上的 handler
+function realFunc() {
+	var detail = event.wheelDelta || event.detail;
+	var moveForwardStep = 1;
+	var moveBackStep = -1;
+	var step = 0;
+	if (detail > 0) {
+		step = moveForwardStep * 400;
+	} else {
+		step = moveBackStep * 400;
+	}
+	let str = gitFlexBox.scrollLeft - step
+	gitFlexBox.scrollTo({
+		left: str,
+		behavior: 'smooth'
+	});
+
+	if ((str > 0) && (str <= 1000)) {
+		gitNowActive = 1
+
+	} else if ((str > 1000) && (str <= 1700)) {
+		gitNowActive = 2
+
+	} else if ((str > 1700) && (str <= 2700)) {
+		gitNowActive = 3
+
+	} else if (str > 2700) {
+		gitNowActive = 4
+
+	} else {
+		console.log(gitNowActive + 'gna')
+	}
+
+	gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
+	gitSub[gitNowActive].addClass('sub-active');
+
+}
+// 采用了节流函数
+//window.addEventListener('scroll',throttle(realFunc,500,1000));
+
+
+
+function scrollGit(n) {
+	gitFlexBox.scrollTo({
+		left: n === 1 ? 0 : n === 2 ? 1350 : n === 3 ? 2400 : n === 4 ? 3600 : 0,
+		behavior: 'smooth'
+	});
+
+	gitNowActive = n;
+	gitSub[n].siblings('.sub-active').removeClass('sub-active')
+	gitSub[n].addClass('sub-active');
+}
+
+
+
+let gitContent = $('#github')
+
+$(document).keydown((event) => {
+
+	let kc = event.keyCode
+	if (gitContent.hasClass('content-item-active')) {
+		if ((kc === 37) || (kc === 65)) {
+			if (gitNowActive != 1) {
+				gitNowActive--;
+				scrollGit(gitNowActive)
+				gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
+				gitSub[gitNowActive].addClass('sub-active');
 			}
-		});
-		
-		
-		let leftSub = $('#leftSub'),rightSub = $('#rightSub')
-		function displaySub(){
-			leftSub.css("opacity",0.9)
-			rightSub.css("opacity",0.9)
-		}
-		function hiddenSub(){
-			leftSub.css("opacity",0)
-			rightSub.css("opacity",0)
-		}
+		} else if ((kc === 39) || (kc === 68)) {
+			if (gitNowActive != 4) {
+				gitNowActive++;
+				scrollGit(gitNowActive)
+				gitSub[gitNowActive].siblings('.sub-active').removeClass('sub-active')
+				gitSub[gitNowActive].addClass('sub-active');
+			}
+		} else {}
+
+
+	}
+
+});
+
+
+let leftSub = $('#leftSub'),
+	rightSub = $('#rightSub')
+
+function displaySub() {
+	leftSub.css("opacity", 0.9)
+	rightSub.css("opacity", 0.9)
+}
+
+function hiddenSub() {
+	leftSub.css("opacity", 0)
+	rightSub.css("opacity", 0)
+}
